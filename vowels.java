c9b151bbcd22524ab95fd8f4cc57ad545dc05df3
deepak2527 <dn2527@gmail.com>
2017-07-09 14:12:50 +0530

    Add files via upload

---
+++ vowels.java
@@ -0,0 +1,17 @@
+class charra
+{
+	public static void main (String[] args) 
+	{
+	char ch;
+	ch='a';
+	if (ch=='a'||ch=='A'||ch=='e'||ch=='E'||ch=='i'||ch=='I'||ch=='o'||ch=='O'||ch=='u'||ch=='U')
+	{
+		System.out.println("vowels");
+		}
+		else
+		{
+			System.out.println("consonants");
+		}
+		
+	}
+}
