e2e5e4536746cc7ea0e58a46c571214102000e07
deepak2527 <dn2527@gmail.com>
2017-07-09 13:34:20 +0530

    Add files via upload

---
+++ even.java
@@ -0,0 +1,17 @@
+class hello
+{
+	public static void main (String[] args) 
+	{
+	int c;
+	c=10;
+	if (c>4)
+	{
+		System.out.println("even");
+		}
+		else
+		{
+			System.out.println("odd");
+		}
+		
+	}
+}
