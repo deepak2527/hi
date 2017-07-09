553cca932a92f82b622ffcbdb700025c44a8f0d1
deepak2527 <dn2527@gmail.com>
2017-07-09 15:48:03 +0530

    Add files via upload

---
+++ natural number.java
@@ -0,0 +1,11 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n;
+		int sum=0;
+		n=4;
+		sum=n*(n-1)/2;
+	System.out.println("natural number");
+	}
+}
