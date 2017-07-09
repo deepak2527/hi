8e48a2c5398bff36cf7ded501ca5892721fef0ff
deepak2527 <dn2527@gmail.com>
2017-07-09 12:39:17 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,20 @@
+class hello
+{
+	public static void main (String[] args) 
+	{
+	int c;
+	c=10;
+	if (c>4)
+	{
+		System.out.println("postive");
+		}
+		else if (c==4)
+		{
+			System.out.println("negative");
+		}
+		else if (c>2)
+		{
+			System.out.println("zero");
+		}
+	}
+}
