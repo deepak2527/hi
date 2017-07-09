82674139748319773c8d23ea708d1044fe4088f2
deepak2527 <dn2527@gmail.com>
2017-07-09 14:42:00 +0530

    Add files via upload

---
+++ number.java
@@ -0,0 +1,12 @@
+class AlphaNum
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	char alphabet;
+	alphabet=1;
+	if(Character.isLetter(alphabet))
+	System.out.println("It's an alphabet");
+	else
+	System.out.println("It's a number");
+	}
+}
