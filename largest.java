e131c0c7a7763a9301ae4b58ac02ab01ac9d2a9b
deepak2527 <dn2527@gmail.com>
2017-07-09 14:49:40 +0530

    Add files via upload

---
+++ largest.java
@@ -0,0 +1,24 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	  int x1,x2,x3;
+   	  int larger;
+	  Scanner s= new Scanner(System.in);
+	  x1=s.nextInt();
+          x2=s.nextInt();
+	  x3=s.nextInt();
+	  {
+	  if(x1>x2)
+         larger=x1;
+     	  else 
+	 larger=x2;
+	}
+	{
+	  if(larger>x3)
+	  System.out.println("larger:"+larger);
+	  else
+	  System.out.println("larger:"+x3);
+	}
+	}
+}
